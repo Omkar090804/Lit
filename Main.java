2fdd0c76b378a33316a2f78f305e0da179a18b37
omkar darekar <omkardarekar0908@gmail.com>
2024-03-06 09:49:31 +0530

    Adding main java

--- Main.java
+++ Main.java
@@ -1,30 +1,35 @@
-﻿// merge sorted arrays
-
-/* class Solution {
+﻿public class Main {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int index1=m-1;
-        int index2=n-1;
-        int mergeindex= m+n-1;
-        while(index1>=0 && index2>=0){
-            if(nums1[index1] > nums2[index2]) {
-                nums1[mergeindex]=nums1[index1];
-                mergeindex--;
-                index1--;
-            }
-            else{
-                nums1[mergeindex]=nums2[index2];
-                mergeindex--;
-                index2--;
+        int index1 = m - 1;
+        int index2 = n - 1;
+        int mergeIndex = m + n - 1;
 
+        while (index1 >= 0 && index2 >= 0) {
+            if (nums1[index1] > nums2[index2]) {
+                nums1[mergeIndex--] = nums1[index1--];
+            } else {
+                nums1[mergeIndex--] = nums2[index2--];
             }
         }
+
         while (index2 >= 0) {
-            nums1[mergeindex--] = nums2[index2--];
+            nums1[mergeIndex--] = nums2[index2--];
+        }
+    }
+
+    public static void main(String[] args) {
+        Main solution = new Main();
+
+        int[] nums1 = new int[]{1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = new int[]{2, 5, 6};
+        int n = 3;
+
+        solution.merge(nums1, m, nums2, n);
+
+        System.out.print("Merged Array: ");
+        for (int num : nums1) {
+            System.out.print(num + " ");
         }
     }
 }
-*/
-
-
-
-

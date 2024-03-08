921413ccdd95be7032b4aacb810f892298b2ffb1
omkar darekar <omkardarekar0908@gmail.com>
2024-03-08 21:51:37 +0530

    fifth question

---
+++ MajorityElement.java
@@ -0,0 +1,19 @@
+class MajorityElement{
+    public int majorityElement(int[] nums) {
+        int cand=0;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                cand= nums[i];
+            }
+
+            if(cand ==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return cand;
+    }
+}

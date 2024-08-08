21611a335b578fd27e45da9d335aaec8aa495dc5
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-08-08 13:18:33 +0530

    Update RemoveDuplicatesfromSortedArray.java

--- RemoveDuplicatesfromSortedArray.java
+++ RemoveDuplicatesfromSortedArray.java
@@ -1,15 +1,27 @@
-class RemoveDuplicatesfromSortedArray{
+class Solution {
     public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        int index = 0;
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] != nums[i + 1]) {
-                nums[index] = nums[i];
-                index++;
+
+        int i = 0; 
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j]; 
             }
         }
-        nums[index] = nums[n - 1];
-        index++;
-        return index;
+
+        return i + 1; 
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+
+        int newLength = solution.removeDuplicates(arr);
+
+        System.out.print("Modified array: ");
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }

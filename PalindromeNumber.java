f49faba66129bd0675bd1983fc4f8ffdebd13262
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-08-18 20:47:47 +0530

    Create PalindromeNumber.java

---
+++ PalindromeNumber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int original=x;
+        int reversed = 0;
+
+        while (x > 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+}

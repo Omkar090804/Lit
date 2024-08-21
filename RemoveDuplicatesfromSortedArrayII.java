5898b97885f301c633fdb3507123c9fe55187683
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-08-21 18:36:31 +0530

    Update RemoveDuplicatesfromSortedArrayII.java

--- RemoveDuplicatesfromSortedArrayII.java
+++ RemoveDuplicatesfromSortedArrayII.java
@@ -1,8 +1,7 @@
 class RemoveDuplicatesfromSortedArrayII {
     public int removeDuplicates(int[] nums) {
-        int n = nums.length;
         int index = 2;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i < nums.length; i++) {
             if (nums[i] != nums[index - 2]) {
                 nums[index] = nums[i];
                 index++;

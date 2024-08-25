26927a7315d677b16d1924f661ba6531661d92a0
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-08-25 13:10:22 +0530

    Update RotateArray.java

--- RotateArray.java
+++ RotateArray.java
@@ -1,31 +1,17 @@
-public class RotateArray {
-    public static void main(String[] args) {
-        RotateArray rotateArray = new RotateArray();
-      
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        int k = 3;
+import java.util.Arrays;
 
-        rotateArray.rotate(nums, k);
-
-        System.out.println("Rotated array:");
-        for (int num : nums) {
-            System.out.print(num + " ");
-        }
-    }
-
+class Solution {
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k % n; // Adjust k if it's larger than the length of the array
+        int n = nums.length;                                   //{1,2,3,4,5,6,7}   k=3;
+       
+        reverse(nums, 0, n - 1);         // Reverse the entire array     {7,6,5,4,3,2,1}
+        reverse(nums, 0, k - 1);         // Reverse the first k elements   {5,6,7,4,3,2,1}}
+        reverse(nums, k, n - 1);         // Reverse the remaining elements    {5,6,7,1,2,3,4}
 
-        // Reverse the entire array
-        reverse(nums, 0, n - 1);
-
-        // Reverse the first k elements
-        reverse(nums, 0, k - 1);
-
-        // Reverse the remaining elements
-        reverse(nums, k, n - 1);
+      
+        System.out.println(Arrays.toString(nums));
     }
+
     private void reverse(int[] nums, int start, int end) {
         while (start < end) {
             int temp = nums[start];

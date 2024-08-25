ba3658276c464134dd196f8c893dfc37b39e9b34
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-08-25 16:31:31 +0530

    Update Besttimetobuyandsellstock.java

--- Besttimetobuyandsellstock.java
+++ Besttimetobuyandsellstock.java
@@ -1,15 +1,19 @@
 class Solution {
-    public int maxProfit(int[] prices) {
+    public int maxProfit(int[] prices)
+    {
         int minPrice = prices[0];
         int maxProfit = 0; 
 
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < minPrice) {
+        for (int i = 0; i < prices.length; i++) 
+        {
+            if (prices[i] < minPrice) 
+            {
                 minPrice = prices[i]; 
-            } else {
-               
-                int profit = prices[i] - minPrice;
-                if (profit > maxProfit) {
+            }
+            else {
+               int profit = prices[i] - minPrice;
+                if (profit > maxProfit) 
+                {
                     maxProfit = profit;
                 }
             }

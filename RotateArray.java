260ac3570fc71e2c974f641df88f5daf1c1edc7b
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-08-25 13:13:49 +0530

    Update RotateArray.java

--- RotateArray.java
+++ RotateArray.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public void rotate(int[] nums, int k) {
-        int n = nums.length;                                   //{1,2,3,4,5,6,7}   k=3;
+        int n = nums.length;                                  //{1,2,3,4,5,6,7}   k=3;
        
         reverse(nums, 0, n - 1);         // Reverse the entire array     {7,6,5,4,3,2,1}
         reverse(nums, 0, k - 1);         // Reverse the first k elements   {5,6,7,4,3,2,1}}

e9bdbda84ac446b18a41066304dc128fadbf7eca
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-08-27 22:35:58 +0530

    Create jumpgame2.java

---
+++ jumpgame2.java
@@ -0,0 +1,26 @@
+class Solution 
+{
+    public int jump(int[] nums)
+  {
+        int jumps = 0;        
+        int currentEnd = 0;     
+        int maxreach = 0;    
+          
+        for (int i = 0; i < nums.length - 1; i++) 
+        {
+            maxreach = Math.max(maxreach, i + nums[i]);
+
+            if (i == currentEnd) 
+            {
+                jumps++;               
+                currentEnd = maxreach; 
+
+                if (currentEnd >= nums.length - 1) 
+                {
+                    return jumps;
+                }
+            }
+        }
+   return jumps; 
+    }
+}

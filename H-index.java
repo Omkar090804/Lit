6890b459a925b568da656649215d02e84d935024
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-08-28 18:12:31 +0530

    Create H-index.java

---
+++ H-index.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution 
+{
+    public int hIndex(int[] citations) 
+  {
+      Arrays.sort(citations);
+        for (int i = 0; i <  citations.length; i++)
+          {
+             int h =  citations.length - i;
+            if (citations[i] >= h)
+            {
+                return h;  
+            }
+        }
+         return 0;  
+    }
+}

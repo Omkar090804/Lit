d572a86e7a51aef2368ecacb477ca41834573708
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-10-09 07:02:18 +0530

    Update mergesortedarray.java

--- mergesortedarray.java
+++ mergesortedarray.java
@@ -1,41 +1,30 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] merge = new int[m + n];
-        int i = 0, j = 0, k = 0;
+        int arr []=new int[m+n];
+        int k=0,i=0,j=0;
 
-        while (i < m && j < n) {
-            if (nums1[i] <= nums2[j]) {
-                merge[k++] = nums1[i++];
-            } else {
-                merge[k++] = nums2[j++];
+        while(m>i && n>j)
+{
+            if(nums1[i]>=nums2[j])
+{
+                arr[k++]=nums2[j++];
+            }
+
+            else{
+                arr[k++]=nums1[i++];
             }
         }
 
-        while (i < m) {
-            merge[k++] = nums1[i++];
+        while(m>i){
+            arr[k++]=nums1[i++];
         }
+        while(n>j){
+            arr[k++]=nums2[j++];
+        }
+     
+     for(int x=0;x < arr.length; x++){
+         nums1[x]=arr[x];
 
-        while (j < n) {
-            merge[k++] = nums2[j++];
-        }
-
-       for (i = 0; i < m + n; i++) {
-            nums1[i] = merge[i];
-        }
-        
+     }
+     }
     }
-
-    public static void main(String[] args) {
-         Solution solution = new Solution();
-        int[] nums1 = {1, 2, 3, 0, 0, 0};
-        int m = 3;
-        int[] nums2 = {2, 5, 6};
-        int n = 3;
-
-        solution.merge(nums1, m, nums2, n);
-
-        for (int i = 0; i < nums1.length; i++) {
-            System.out.print(nums1[i] + " ");
-        }
-    }
-}

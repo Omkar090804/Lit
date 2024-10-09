2623be2c30abce879195ce17f89299b87fcdf76f
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-10-09 07:37:35 +0530

    Update RemoveElement.java

--- RemoveElement.java
+++ RemoveElement.java
@@ -8,18 +8,5 @@ class Solution {
             }
         }
         return k;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] nums = {1, 2, 3, 5, 4, 5};
-        int val = 5;
-        
-        int newLength = solution.removeElement(nums, val);
-        
-        System.out.print("Modified array: ");
-       for (int i = 0; i < newLength; i++) {
-            System.out.print(nums[i] + " ");
-        }
-    }
+    }  
 }

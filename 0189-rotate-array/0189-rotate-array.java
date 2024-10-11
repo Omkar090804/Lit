dc5cb9804ccbdc8f398963bc0b417d0455ee708d
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-10-11 08:12:55 +0530

    Time: 0 ms (100.00%), Space: 57 MB (88.75%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;                                 
-         k = k % n; 
+         k = k % n;                //to ensure k is within array bounds
         reverse(nums, 0, n - 1);      
         reverse(nums, 0, k - 1);        
         reverse(nums, k, n - 1);         
